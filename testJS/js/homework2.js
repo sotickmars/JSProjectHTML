9eec129fb00ddd89822cde96df5aa90c8a37badd
Ilya_Kont <38035777+sotickmars@users.noreply.github.com>
2020-03-21 13:04:24 +0300

    add classwork

--- testJS/js/homework2.js
+++ testJS/js/homework2.js
@@ -1,26 +1,71 @@
-let myAge = Number(prompt('Сколько вам лет?'));
-let retirementAge = (65 - myAge);
+// let myAge = Number(prompt('Сколько вам лет?'));
+// let retirementAge = (65 - myAge);
 
-if (myAge >= 0 && myAge < 65) {
-    alert(`Вам осталось ${retirementAge} до пенсии`);
-} else if (myAge < 0) {
-    alert(`Введите заного значение`);
-}
-else {
-    alert('Вы уже на пенсии');
-};
+// if (myAge >= 0 && myAge < 65) {
+//     alert(`Вам осталось ${retirementAge} до пенсии`);
+// } else if (myAge < 0) {
+//     alert(`Введите заного значение`);
+// }
+// else {
+//     alert('Вы уже на пенсии');
+// };
 
 
-switch (true) {
-    case myAge >= 0 && myAge < 65:
-        alert(`Вам осталось ${retirementAge} до пенсии`);
-        break;
+// switch (true) {
+//     case myAge >= 0 && myAge < 65:
+//         alert(`Вам осталось ${retirementAge} до пенсии`);
+//         break;
 
-    case myAge < 0:
-        alert(`Введите заного значение`);
-        break;
+//     case myAge < 0:
+//         alert(`Введите заного значение`);
+//         break;
   
-    default :
-        alert('Вы уже на пенсии');
-        break;
+//     default :
+//         alert('Вы уже на пенсии');
+//         break;
+// }
+
+
+
+// let counter = 0;
+
+// do {
+//     if (counter % 2  !== 0){
+//     console.log(-counter)
+//     }
+//     else {
+//         console.log(counter);
+//     }
+//     ++counter;
+// } while (counter<10);
+
+
+
+// let num1 = 0;
+// let num2 = -9;
+// do {
+//     console.log(num1);
+//     num1++;
+//     console.log(num2);
+//     num2++;
+// } while (num1<5);
+
+
+
+
+// let num = -9;
+// do {
+//     console.log(num+9);
+//     console.log(0 + num);
+//     num++;
+
+// }while (num <= -5);
+
+
+
+
+for(let num = -9; num < -4; ++num){
+    console.log(num+9);
+    console.log(0 + num);
+    // if(num >= -5) break;
 }
